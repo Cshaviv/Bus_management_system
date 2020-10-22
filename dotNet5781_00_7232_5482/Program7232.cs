dfdabb344d20129ce7185bf3c2c15fbd460db722
ILYLP <ILYLP@DESKTOP-MJT6RUB>
2020-10-22 11:48:41 +0300

    add func welcome7232

---
+++ dotNet5781_00_7232_5482/Program7232.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7232_5482
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7232();
+            Welcome5482();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome5482();
+        private static void Welcome7232()
+        {
+            Console.Write("Enter your name: ");
+            string str;
+            str = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", str);
+        }
+    }
+}
---
+++ dotNet5781_00_7232_5482/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dotNet5781_00_7232_5482")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("dotNet5781_00_7232_5482")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fedbfc86-cdb3-4bac-999e-6a6b4851c8b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
