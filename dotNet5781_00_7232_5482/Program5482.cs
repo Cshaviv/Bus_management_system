b7c350ac2caa7b15ca71785b829e6a6a1d38311e
Chagit Shaviv <chagit9555@gmail.com>
2020-10-22 12:17:43 +0300

    add welcome5482

---
+++ dotNet5781_00_7232_5482/Program5482.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7232_5482
+{
+    partial class Program
+    {
+        static partial void Welcome5482()
+        {
+            Console.Write("Hi I am here");
+
+          
+            
+        }
+    }
+}
