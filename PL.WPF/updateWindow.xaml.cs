42836fdc84857052c1c503b3a546fea3c7d41c4f
ILYLP <ILYLP@DESKTOP-MJT6RUB>
2021-01-06 01:51:25 +0200

    cvbn

--- PL.WPF/updateWindow.xaml.cs
+++ PL.WPF/updateWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BLAPI;
+using BO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +25,21 @@ namespace PL.WPF
         public updateWindow(Bus b, IBL _bl)
         {
             InitializeComponent();
-            bl = _bL;
+            bl = _bl;
+            licenseNumTextBox.Text = b.LicenseNum.ToString();
+            startDateDatePicker.Text = b.StartDate.Day + "/" + b.StartDate.Month + "/" + b.StartDate.Year;
+            lastTreatDatePicker.Text = b.StartDate.Day + "/" + b.StartDate.Month + "/" + b.StartDate.Year;
+            kmTextBox.Text = b.TotalKm.ToString();
+            kmafterrefuelingTextBox.Text = b.KmLastTreat.ToString();
+            kmaftertreatTextBox.Text = b.kmAfterRefuling.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busViewSource.Source = [generic data source]
         }
     }
 }
